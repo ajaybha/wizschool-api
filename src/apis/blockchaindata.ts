f6459c77d7afda7977f1076794ef6582cfdebdfe
Ajay <ajay.garhwal@outlook.com>
2024-07-23 19:14:01 +0530

    just loggin json from callback

--- src/apis/blockchaindata.ts
+++ src/apis/blockchaindata.ts
@@ -1,7 +1,31 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import {config, blockchainData} from '@imtbl/sdk';
+
+const PublishableKey = process.env.PUBLISHABLE_KEY;
+const client = new blockchainData.BlockchainData({
+    baseConfig: {
+        environment: config.Environment.SANDBOX,
+        publishableKey: PublishableKey
+    }
+});
+// constants for polling request parameters
+const chainName: string = process.env.SANDBOX_CHAIN_NAME ? process.env.SANDBOX_CHAIN_NAME : "";
+const contractAddress: string = process.env.SANDBOX_DEFAULT_COLLECTION_CONTRACT_ADDR ? process.env.SANDBOX_DEFAULT_COLLECTION_CONTRACT_ADDR : "";
 
 const pollActivities = async () => {
     console.log("polling blockchaindata api for chain activity");
+    
+    const response = await client.listActivities({
+        chainName,
+        contractAddress
+    });
+    
+    // filter only the mint activity    
+    response.result.filter(result => result.type == 'mint');
+    console.log("Filtered mint activities");
+    console.log(JSON.stringify(response, null, "\t"));
+    
 };
 
 export {pollActivities};
